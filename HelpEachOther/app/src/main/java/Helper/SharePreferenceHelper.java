0c5f8bcd8d6a783231f8571a6cc4a88ee74c95bc
codeDogDream <503963720@qq.com>
2016-11-07 20:17:41 +0800

    删除部分东西

--- HelpEachOther/app/src/main/java/Helper/SharePreferenceHelper.java
+++ HelpEachOther/app/src/main/java/Helper/SharePreferenceHelper.java
@@ -8,7 +8,6 @@ import com.google.gson.JsonObject;
 
 public class SharePreferenceHelper {
 
-	public static final String PREFERENCE_LIVE_ROOM_INFO = "live_room_info";
 
 	public static void saveSharePreferenceFromString(Context context, String name, String key, String value) {
 		SharedPreferences sharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE);
@@ -64,57 +63,5 @@ public class SharePreferenceHelper {
 
 	}
 
-	/**
-	 * 保存用户信息
-	 *
-	 * @param jo
-	 * @param x  1代表登录，2代表获取用户信息
-	 */
-	public static boolean saveUserInfo(Context context, JsonObject jo, int x) {
-		switch (x) {
-			case 1:
-				try {
-					SharePreferenceHelper.saveSharePreferenceFromBoolean(context, "user_info", "isLogin", true);
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "uid", jo.get("uid").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "avatar", jo.get("avatar").getAsString());
-					SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "nickname", jo.get("nickname").getAsString());
-					SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "avatar_big", jo.get("avatar_big").getAsString());
-					if (jo.has("token")) {
-						jo = jo.get("token").getAsJsonObject();
-						SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "login_token", jo.get("login_token").getAsString());
-						SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "refresh_token", jo.get("refresh_token").getAsString());
-						SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "login_token_expiresin", (int) (System.currentTimeMillis() / 1000 + jo.get("login_token_expiresin").getAsInt()));
-						SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "refresh_token_expiresin", (int) (System.currentTimeMillis() / 1000 + jo.get("refresh_token_expiresin").getAsInt()));
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-					return false;
-				}
-				break;
-			case 2:
-				try {
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "uid", jo.get("uid").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "avatar", jo.get("avatar").getAsString());
-					SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "nickname", jo.get("nickname").getAsString());
-					SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "avatar_big", jo.get("avatar_big").getAsString());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "friends_count", jo.get("friends_count").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "followers_count", jo.get("followers_count").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "level", jo.get("level").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "exp", jo.get("exp").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "verified", jo.get("verified").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "tickets", jo.get("tickets").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "history_tickets", jo.get("history_tickets").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "sent_tickets", jo.get("sent_tickets").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromInteger(context, "user_info", "coins", jo.get("coins").getAsInt());
-					SharePreferenceHelper.saveSharePreferenceFromString(context, "user_info", "gender", jo.get("gender").getAsString());
-				} catch (Exception e) {
-					e.printStackTrace();
-					return false;
-				}
-				break;
-
-		}
-		return true;
-	}
 
 }
